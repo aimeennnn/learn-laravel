cc91dbc75f63a28201253e63658f1358218dba62
aimeennnn <muh.aiman21304@gmail.com>
2024-04-25 11:15:17 +0700

    Perubahan Validasi Login

--- app/Http/Controllers/SesiController.php
+++ app/Http/Controllers/SesiController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SesiController extends Controller
 {
     function index()
     {
         return view('login');
+    }
+    function login(Request $request)
+    {
+        $request -> validate([
+            'email' => 'required',
+            'password' => 'required'
+        ],[
+            'email.required' => 'Email Harus Diisi!!',
+            'password.required' => 'Password Harus Diisi',
+        ]);
+
+        $validasilogin = [
+            'email'=>$request -> email,
+            'password'=>$request -> password
+        ];
+
+        if(Auth::attempt($validasilogin))
+        {
+            echo "Succes";exit();
+        }else{
+            return redirect('')->withErrors('Username Atau Password Yang Dimasukkan Tidak Sesusai!!')->withInput();
+        }
     }
 }
